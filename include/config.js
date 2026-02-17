88cd061033cd9244d01dd2ac732d7dc94bdaccb9
rob.hawthorne@trademe.co.nz <rob.hawthorne@trademe.co.nz>
2026-02-17 07:58:41 +0000

    Changing to be dynamic across multiple datasets

--- include/config.js
+++
@@ -1,8 +0,0 @@
-// includes/config.js
-module.exports = {
-  project_id: "trademebigquery2",
-  source_dataset_prefix: "analytics_",
-  target_dataset: "ingest_marketing_analytics",
-  target_table: "events_consolidated",
-  lookback_days: 2
-};
